de7d8ee603b80b5bfc3b42cbe5e22de2d9d20625
Yana Lapach <y.lapach@gmail.com>
2023-03-30 18:19:00 +0200

    coutry search

--- src/index.js
+++ src/index.js
@@ -1,3 +1,77 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { fetchCountries } from './js/fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+
+const searchEl = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+const cleanMarkup = ref => (ref.innerHTML = '');
+
+const inputHandler = e => {
+  const textInput = e.target.value.trim();
+
+  if (!textInput) {
+    cleanMarkup(countryList);
+    cleanMarkup(countryInfo);
+    return;
+  }
+
+  fetchCountries(textInput)
+    .then(data => {
+      console.log(data);
+      if (data.length > 10) {
+        Notify.info(
+          'Too many matches found. Please enter a more specific name'
+        );
+        return;
+      }
+      renderMarkup(data);
+    })
+    .catch(err => {
+      cleanMarkup(countryList);
+      cleanMarkup(countryInfo);
+      Notify.failure('Oops, there is no country with that name');
+    });
+};
+
+const renderMarkup = data => {
+  if (data.length === 1) {
+    cleanMarkup(countryList);
+    const markupInfo = createInfoMarkup(data);
+    countryInfo.innerHTML = markupInfo;
+  } else {
+    cleanMarkup(countryInfo);
+    const markupList = createListMarkup(data);
+    countryList.innerHTML = markupList;
+  }
+};
+
+const createListMarkup = data => {
+  return data
+    .map(
+      ({ name, flags }) =>
+        `<li><img class="flag" src="${flags.png}" alt="${name.official}">${name.official}</li>`
+    )
+    .join('');
+};
+
+const createInfoMarkup = data => {
+  return data.map(
+    ({ name, capital, population, flags, languages }) =>
+      `<div class="country-profile">
+      <img class="country-flag" src="${flags.png}" alt="${name.official}">
+      <h2>${name.official}</h2>
+      <ul class="country-data">
+      <li>Capital: ${capital}</li>
+      <li>Population: ${population}</li>
+      <li>Languages: ${Object.values(languages)}</li>
+      </ul>
+      </div>`
+  );
+};
+
+searchEl.addEventListener('input', debounce(inputHandler, DEBOUNCE_DELAY));
